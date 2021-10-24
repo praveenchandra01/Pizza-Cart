99643d3bd8fee1d8f7312420cd9feea3635cd4e4
Praveen Chandra <praveen@bitcs.in>
2021-10-24 23:44:06 +0530

    add scroll effect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,13 @@ const Home = () => {
     const btn = {
         transition: 'all .3s'
     }
+    function scrollBottom(){
+        window.scroll({
+            top: 650,
+            left: 0, 
+            behavior: 'smooth',
+          });
+    }
 
     return (
         <>
@@ -14,7 +21,7 @@ const Home = () => {
                 <div className="p-6 md:pl-12 w-1/2">
                 <h6 className="text-lg md:text-3xl"><em>Are you hungry?</em></h6>
                 <h1 className="text-3xl md:text-7xl font-bold">Don't wait !</h1>
-                <button style={btn} className="px-3 py-1 sm:px-6 sm:py-2 text-sm sm:text-lg rounded-full text-white font-bold mt-2 sm:mt-4 bg-yellow-500 hover:bg-yellow-400">Order Now</button>
+                <button style={btn} onClick={scrollBottom} className="px-3 py-1 sm:px-6 sm:py-2 text-sm sm:text-lg rounded-full text-white font-bold mt-2 sm:mt-4 bg-yellow-500 hover:bg-yellow-400">Order Now</button>
                 </div>
                 <div className="w-1/2">
                     <img className="w-5/6 m-auto py-10" src={homePizza} alt="Pizza" />
